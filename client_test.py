1fb7a73bc98ec4e2b36ef3b36407bb60deab125a
Farhan Tanvir <farhantaanvir@gmail.com>
2024-03-28 03:53:32 +0600

    made necessary changes

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint,getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +8,10 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      top_ask=quote['top_ask']['price']
+      top_bid=quote['top_bid']['price']
+      self.assertEqual(getDataPoint(quote),(quote['stock'],top_bid,top_ask,(top_ask+top_bid)/2))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,10 +19,23 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    for quote in quotes:
+      top_ask=quote['top_ask']['price']
+      top_bid=quote['top_bid']['price']
+      self.assertEqual(getDataPoint(quote),(quote['stock'],top_bid,top_ask,(top_ask+top_bid)/2))
 
   """ ------------ Add more unit tests ------------ """
-
+  def test_getRatio(self):
+    quotes = [
+      {'top_ask': {'price': 0, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 15.25, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 0, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    """ ------------ Add the assertion below ------------ """
+    prices={}
+    for quote in quotes:
+      stock, bid_price, ask_price, price = getDataPoint(quote)
+      prices[stock]=price
+    self.assertEqual(getRatio(prices['ABC'],prices['DEF']), prices['ABC']/prices['DEF'] if prices['DEF']!=0 else None)
 
 
 if __name__ == '__main__':
